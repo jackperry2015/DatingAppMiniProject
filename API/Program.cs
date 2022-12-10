2f669da6892d117f8eb709c433e105e361ddf6c0
Jack Perry <jack.perry.2015@my.ntu.ac.uk>
2022-12-10 00:04:11 +0000

    Angular Page Init

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
+
 
 var app = builder.Build();
 
@@ -22,6 +24,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
